3dd14918828f042be9d17c65f76fa5c256c4dee4
Jiwon Park <xoxojw.dev@gmail.com>
2023-06-23 12:31:04 +0900

    :package: 각 기능별로 컴포넌트 파일 분리(button, input, modal, select)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import GlobalStyle from "GlobalStyle";
+import Modal from "components/Modal";
+import Button from "components/Button";
+import Input from "components/Input";
+import Select from "components/Select";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <Button />
+      <Input />
+      <Modal />
+      <Select />
+    </>
+  )
 };
 
 export default App;
