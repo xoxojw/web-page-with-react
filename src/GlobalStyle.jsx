91e086c4374e94ad7fb0923c449056ffee427579
Jiwon Park <xoxojw.dev@gmail.com>
2023-06-23 12:31:26 +0900

    :lipstick: 전역 스타일링 elements 설정

---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  :root {
+    --main-ft-color: #1a2431;
+  }
+
+  body {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: var(--main-ft-color);
+  }
+
+  h1 {
+    display: block;
+    margin-block-start: 2vh;
+    margin-block-end: 2vh;
+    font-size: 2em;
+    font-weight: 700;
+  }
+
+  div {
+    margin: 1vh;
+  }
+  
+`;
+
+export default GlobalStyle;
